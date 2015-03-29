779fd21087d767c9e720194be7351d85f853784f
Adrian-lab <lianke123321@gmail.com>
2015-03-29 14:11:56 -0400

    a simple echo server

--- hw3/pbproxy.c
+++ hw3/pbproxy.c
@@ -14,16 +14,16 @@ typedef enum { false, true } boolean;
 
 int main(int argc, char *argv[]) {
 	int opt = 0;
-	char *listen_port = NULL;
+	char *str_listen_port = NULL;
 	boolean server_mode = false;
 	char *key_file = NULL;
-	char *dst = NULL;
-	char *dst_port = NULL;
+	char *str_dst = NULL;
+	char *str_dst_port = NULL;
 	
 	while ((opt = getopt(argc, argv, "l:k:")) != -1) {
 		switch(opt) {
 			case 'l':
-				listen_port = optarg;
+				str_listen_port = optarg;
 				server_mode = true;
 				//printf("listen_port: %s\n", listen_port);
 				break;
@@ -51,8 +51,8 @@ int main(int argc, char *argv[]) {
 	
 	// get destination ip and port
 	if (optind == argc - 2) {
-		dst = argv[optind];
-		dst_port = argv[optind+1];
+		str_dst = argv[optind];
+		str_dst_port = argv[optind+1];
 	} else {
 		printf("optind: %d, argc: %d\n", optind, argc);
 		printf("Incorrect destination and port arguments. Exiting...\n");
@@ -70,6 +70,66 @@ int main(int argc, char *argv[]) {
 		key file: %s\n\
 		destination addr: %s\n\
 		destination port: %s\n\n\n"\
-		, server_mode ? "true" : "false", listen_port, key_file,\
-		dst, dst_port);
+		, server_mode ? "true" : "false", str_listen_port, key_file,\
+		str_dst, str_dst_port);
+	
+	int dst_port = (int)strtol(str_dst_port, NULL, 10);
+	struct sockaddr_in servaddr;
+	bzero(&servaddr, sizeof(servaddr));
+	
+	// pbproxy running in server mode
+	if (server_mode == true) {
+		char str[100];
+		int listen_fd, comm_fd;
+		int listen_port = (int)strtol(str_listen_port, NULL, 10);
+		listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+		
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+		servaddr.sin_port = htons(listen_port);
+		
+		bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+		
+		listen(listen_fd, 10);
+		
+		comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
+		
+		while (1) {
+			bzero(str, 100);
+			read(comm_fd, str, 100);
+			printf("Echoing back - %s",str);
+			write(comm_fd, str, strlen(str)+1);
+		}
+	} else {
+		// pbproxy running in client mode
+		int sockfd,n;
+		char sendline[100];
+		char recvline[100];
+		
+		sockfd=socket(AF_INET,SOCK_STREAM,0);
+		
+		servaddr.sin_family=AF_INET;
+		servaddr.sin_port=htons(dst_port);
+		
+		inet_pton(AF_INET, str_dst, &(servaddr.sin_addr));
+		
+		if (connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)) == -1) {
+			printf("Connect Error!\n");
+		} else {
+			printf("Connection established!\n");
+		}
+		
+		while(1)
+		{
+			bzero(sendline, 100);
+			bzero(recvline, 100);
+			fgets(sendline,100,stdin); /*stdin = 0 , for standard input */
+			
+			write(sockfd,sendline,strlen(sendline)+1);
+			read(sockfd,recvline,100);
+			printf("%s",recvline);
+		}
+	}
+	
+	return 1;
 }
