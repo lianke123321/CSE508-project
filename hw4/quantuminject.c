b2d6a2a389a8b2cb4b0f95c84e4cc91f2c0422f6
Adrian-lab <lianke123321@gmail.com>
2015-04-19 22:22:09 -0400

    now there is a buggy spoof packet sender

--- hw4/quantuminject.c
+++ hw4/quantuminject.c
@@ -14,12 +14,31 @@
 
 #define ETHER_ADDR_LEN 6
 #define SIZE_ETHERNET 14
-#define IP_HL(ip) (((ip)->ip_vhl) & 0x0f)
-#define IP_V(ip) (((ip)->ip_vhl) >> 4)
 #define ETHERTYPE_ARP 0x0806
 #define ETHERTYPE_IPV4 0x0800
 
 typedef enum { false, true } boolean;
+
+unsigned char *spoof_payload = NULL;
+
+char* read_file(const char* filename) {
+	char *buffer = 0;
+	long length;
+	FILE *f = fopen (filename, "rb");
+	
+	if (f) {
+		fseek (f, 0, SEEK_END);
+		length = ftell (f);
+		fseek (f, 0, SEEK_SET);
+		buffer = malloc (length);
+		if (buffer)
+			fread (buffer, 1, length, f);
+		fclose (f);
+	} else
+		return 0;
+	
+	return buffer;
+}
 
 // define necessary struct for headers
 /* Ethernet header */
@@ -29,10 +48,11 @@ struct sniff_ethernet
 	u_char  ether_shost[ETHER_ADDR_LEN];	/* source host address */
 	u_short ether_type;					 /* IP? ARP? RARP? etc */
 };
+
 /* IP header */
 struct sniff_ip
 {
-	u_char  ip_vhl;				 /* version << 4 | header length >> 2 */
+	u_char  ip_hl:4, ip_v:4;				 /* version << 4 | header length >> 2 */
 	u_char  ip_tos;				 /* type of service */
 	u_short ip_len;				 /* total length */
 	u_short ip_id;				  /* identification */
@@ -46,23 +66,17 @@ struct sniff_ip
 	u_short ip_sum;				 /* checksum */
 	struct  in_addr ip_src,ip_dst;  /* source and dest address */
 };
-/* UDP header */
-struct sniff_udp {
-	u_short sport;	/* source port */
-	u_short dport;	/* destination port */
-	u_short udp_length;
-	u_short udp_sum;	/* checksum */
-};
+
 /* TCP header */
 typedef u_int tcp_seq;
+
 struct sniff_tcp
 {
 	u_short th_sport;			   /* source port */
 	u_short th_dport;			   /* destination port */
 	tcp_seq th_seq;				 /* sequence number */
 	tcp_seq th_ack;				 /* acknowledgement number */
-	u_char  th_offx2;			   /* data offset, rsvd */
-#define TH_OFF(th)	  (((th)->th_offx2 & 0xf0) >> 4)
+	u_char  th_x2:4, th_off:4;			   /* data offset, rsvd */
 	u_char  th_flags;
 #define TH_FIN  0x01
 #define TH_SYN  0x02
@@ -78,128 +92,101 @@ struct sniff_tcp
 	u_short th_urp;				 /* urgent pointer */
 };
 
-void print_hex_ascii_line(const u_char *payload, int len, int offset) {
-	int i;
-	int gap;
-	const u_char *ch;
-	
-	/* offset */
-	printf("%05d   ", offset);
-	
-	/* hex */
-	ch = payload;
-	for(i = 0; i < len; i++) {
-		printf("%02x ", *ch);
-		ch++;
-		/* print extra space after 8th byte for visual aid */
-		if (i == 7)
-			printf(" ");
-	}
-	/* print space to handle line less than 8 bytes */
-	if (len < 8)
-		printf(" ");
-
-	/* fill hex gap with spaces if not full line */
-	if (len < 16) {
-		gap = 16 - len;
-		for (i = 0; i < gap; i++) {
-			printf("   ");
-		}
-	}
-	printf("   ");
-
-	/* ascii (if printable) */
-	ch = payload;
-	for(i = 0; i < len; i++) {
-		if (isprint(*ch))
-			printf("%c", *ch);
-		else
-			printf(".");
-		ch++;
-	}
-
-	printf("\n");
-
-	return;
+// for generating checksum of packet
+unsigned short csum (unsigned short *buf, int nwords) {
+	unsigned long sum;
+	for (sum = 0; nwords > 0; nwords--)
+		sum += *buf++;
+	sum = (sum >> 16) + (sum & 0xffff);
+	sum += (sum >> 16);
+	return ~sum;
 }
 
-void print_payload(const u_char *payload, int len) {
-	int len_rem = len;
-	int line_width = 16;	/* number of bytes per line */
-	int line_len;
-	int offset = 0;		/* zero-based offset counter */
-	const u_char *ch = payload;
-
-	if (len <= 0)
-		return;
-
-	/* data fits on one line */
-	if (len <= line_width) {
-		print_hex_ascii_line(ch, len, offset);
-		return;
+int send_spoof_packet () {
+	int raw_fd = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);
+	char datagram[4096];
+	struct sniff_ip *ip_header = (struct sniff_ip *)datagram;
+	struct sniff_tcp *tcp_header = (struct sniff_tcp *)(datagram + sizeof(struct sniff_ip));
+	struct sockaddr_in sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons (25);
+	sin.sin_addr.s_addr = inet_addr ("10.0.1.6");
+	
+	memset(datagram, 0, 4096);
+	
+	/* we'll now fill in the ip/tcp header values, see above for explanations */
+	ip_header->ip_hl = 5;
+	ip_header->ip_v = 4;
+	ip_header->ip_tos = 0;
+	ip_header->ip_len = sizeof(struct sniff_ip) + sizeof(struct sniff_tcp);	/* no payload */
+	ip_header->ip_id = htonl (54321);	/* the value doesn't matter here */
+	ip_header->ip_off = 0;
+	ip_header->ip_ttl = 255;
+	ip_header->ip_p = 6;
+	ip_header->ip_sum = 0;		/* set it to 0 before computing the actual checksum later */
+	ip_header->ip_src.s_addr = inet_addr ("8.8.8.8");/* SYN's can be blindly spoofed */
+	ip_header->ip_dst.s_addr = sin.sin_addr.s_addr;
+	tcp_header->th_sport = htons (1234);	/* arbitrary port */
+	tcp_header->th_dport = htons (25);
+	tcp_header->th_seq = random();/* in a SYN packet, the sequence is a random */
+	tcp_header->th_ack = 0;/* number, and the ack sequence is 0 in the 1st packet */
+	tcp_header->th_x2 = 0;
+	tcp_header->th_off = 0;		/* first and only tcp segment */
+	tcp_header->th_flags = TH_SYN;	/* initial connection request */
+	tcp_header->th_win = htonl (65535);	/* maximum allowed window size */
+	tcp_header->th_sum = 0;/* if you set a checksum to zero, your kernel's IP stack
+		      should fill in the correct checksum during transmission */
+	tcp_header->th_urp = 0;
+	
+	ip_header->ip_sum = csum ((unsigned short *)datagram, ip_header->ip_len >> 1);
+	
+/* finally, it is very advisable to do a IP_HDRINCL call, to make sure
+   that the kernel knows the header is included in the data, and doesn't
+   insert its own header into the packet before our data */
+	
+	/* lets do it the ugly way.. */
+	int one = 1;
+	const int *val = &one;
+	if (setsockopt (raw_fd, IPPROTO_IP, IP_HDRINCL, val, sizeof(one)) < 0)
+		printf ("Warning: Cannot set HDRINCL!\n");
+	
+	if (sendto(raw_fd,		/* our socket */
+		datagram,	/* the buffer containing headers and data */
+		ip_header->ip_len,	/* total length of our datagram */
+		0,		/* routing flags, normally always 0 */
+		(struct sockaddr *) &sin,	/* socket addr, just like in */
+		sizeof (sin)) < 0) {		/* a normal send() */
+		//printf ("Send spoof packet error\n");
+		close(raw_fd);
+		return 0;
+	} else {
+		//printf ("Send spoof packet succeed!\n");
+		close(raw_fd);
+		return 1;
 	}
-
-	/* data spans multiple lines */
-	for ( ;; ) {
-		/* compute current line length */
-		line_len = line_width % len_rem;
-
-		/* print line */
-		print_hex_ascii_line(ch, line_len, offset);
-
-		/* compute total remaining */
-		len_rem = len_rem - line_len;
-
-		/* shift pointer to remaining bytes to print */
-		ch = ch + line_len;
-
-		/* add offset */
-		offset = offset + line_width;
-
-		/* check if we have line width chars or less */
-		if (len_rem <= line_width) {
-			/* print last line and get out */
-			print_hex_ascii_line(ch, len_rem, offset);
-			break;
-		}
-	}
-
-	return;
 }
 
 // display info for each packet
-void handle_packet(boolean http, char *string, const struct pcap_pkthdr *header,\
-	const u_char *packet) {
+void handle_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
 	/* declare pointers to packet headers */
 	const struct sniff_ethernet *ethernet;
 	const struct sniff_ip *ip;
 	const struct sniff_tcp *tcp;
-	const struct sniff_udp *udp;
 	const char *payload;
 	
 	int size_ip;
 	int size_tcp;
-	int size_udp = 8; // fixed udp header length
-	int size_icmp = 8; // fixed udp header length
 	int size_payload;
-	
-	// print out time stamp. convert elapse time to date,
-	// then remove new line symbol at the end
-	time_t raw_time = (time_t)header->ts.tv_sec;
-	char *ptr = ctime(&raw_time);
-	char timebuf[126];
-	strcpy(timebuf, ptr);
-	timebuf[strlen(timebuf)-1] = 0;
-	printf("%s ", timebuf);
 	
 	// extract ethernet header
 	ethernet = (struct sniff_ethernet*)(packet);
 	
 	if (ntohs(ethernet->ether_type) == ETHERTYPE_IPV4) {
-		printf("IPv4 ");
+		//printf("regexp: %s\n", args);
+		//printf("IPv4 ");
 		// extract ip header
 		ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
-		size_ip = IP_HL(ip)*4;
+		size_ip = ip->ip_hl*4;
 		if (size_ip < 20) {
 			printf("* Invalid IP header length: %u bytes\n", size_ip);
 			return;
@@ -207,16 +194,16 @@ void handle_packet(boolean http, char *string, const struct pcap_pkthdr *header,
 		
 		// for tcp packet
 		if (ip->ip_p == IPPROTO_TCP) {
-			printf("TCP ");
-			tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
-			size_tcp = TH_OFF(tcp)*4;
+			//printf("TCP ");
+			tcp = (struct sniff_tcp *)(packet + SIZE_ETHERNET + size_ip);
+			size_tcp = tcp->th_off * 4;
 			if (size_tcp < 20) {
 				printf("* Invalid TCP header length: %u bytes\n", size_tcp);
 				return;
 			}
-			printf("%s.%d -> ", inet_ntoa(ip->ip_src), ntohs(tcp->th_sport));
-			printf("%s.%d ", inet_ntoa(ip->ip_dst), ntohs(tcp->th_dport));
-			printf("len %d ", ntohs(ip->ip_len));
+			//printf("%s.%d -> ", inet_ntoa(ip->ip_src), ntohs(tcp->th_sport));
+			//printf("%s.%d ", inet_ntoa(ip->ip_dst), ntohs(tcp->th_dport));
+			//printf("len %d ", ntohs(ip->ip_len));
 			
 			// extract payload
 			payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
@@ -224,212 +211,39 @@ void handle_packet(boolean http, char *string, const struct pcap_pkthdr *header,
 			
 			// print payload
 			if (size_payload > 0) {
-				printf("Payload (%d bytes):\n", size_payload);
+				//printf("Payload (%d bytes)\n", size_payload);
 				
-				if (string != NULL) {
-					if (strstr(payload, string) == NULL)
+				if (args != NULL) {
+					if (strstr(payload, args) == NULL)
 						return;
-				}
-				
-				if (http) {
-					char tmp[strlen(payload)];
-					strcpy(tmp, payload);
-					char *ptr = strtok(tmp, " ");
-					ptr = strtok(NULL, " ");
-					printf("%s\n", ptr);
-				} else {
-					print_payload(payload, size_payload);
-				}
-			}
-			printf("\n");
-		} else if (ip->ip_p == IPPROTO_UDP) {
-			printf("UDP ");
-			udp = (struct sniff_udp*)(packet + SIZE_ETHERNET + size_ip);
-			
-			printf("%s.%d -> ", inet_ntoa(ip->ip_src), ntohs(udp->sport));
-			printf("%s.%d ", inet_ntoa(ip->ip_dst), ntohs(udp->dport));
-			printf("len %d ", ntohs(ip->ip_len));
-			
-			// extract payload
-			payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_udp);
-			size_payload = ntohs(ip->ip_len) - (size_ip + size_udp);
-			
-			// print payload
-			if (size_payload > 0)
-			{
-				printf("Payload (%d bytes):\n", size_payload);
-				print_payload(payload, size_payload);
-			}
-			printf("\n");
-		} else if (ip->ip_p == IPPROTO_ICMP) {
-			printf("ICMP ");
-			
-			printf("%s -> ", inet_ntoa(ip->ip_src));
-			printf("%s ", inet_ntoa(ip->ip_dst));
-			printf("len %d ", ntohs(ip->ip_len));
-			
-			// extract payload
-			payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_icmp);
-			size_payload = ntohs(ip->ip_len) - (size_ip + size_icmp);
-			
-			// print payload
-			if (size_payload > 0)
-			{
-				printf("Payload (%d bytes):\n", size_payload);
-				print_payload(payload, size_payload);
-			}
-			printf("\n");
-		} else {
-			printf("OTHER ");
-			// extract payload
-			payload = (u_char *)(packet + SIZE_ETHERNET + size_ip);
-			size_payload = ntohs(ip->ip_len) - (size_ip);
-			
-			// print payload
-			if (size_payload > 0)
-			{
-				printf("Payload (%d bytes):\n", size_payload);
-				print_payload(payload, size_payload);
-			}
-			printf("\n");
-		}
-	} else if (ntohs(ethernet->ether_type) == ETHERTYPE_ARP) {
-		printf("ARP\n");
-	} else {
-		printf("OTHER\n");
-	}
-	
-	return;
-}
-
-// callback function for pcap_loop
-void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
-	// parse args and initialize http and string
-	//printf("got packet!\n");
-	boolean http = false;
-	char *string = NULL;
-	if (args != NULL) {
-		char g = *args;
-		if (g == 'g') {
-			http = true;
-			if (strlen(args+1) > 0) {
-				string = args + 1;
-				//printf("There is pattern string!\n");
-			}
-			//printf("New string: %s\n", string);
-		} else {
-			string = args;
-			//printf("New string: %s\n", string);
-		}
-	}
-	
-	if (string == NULL)
-		handle_packet(http, string, header, packet);
-	else {
-		/* declare pointers to packet headers */
-		const struct sniff_ethernet *ethernet;
-		const struct sniff_ip *ip;
-		const struct sniff_tcp *tcp;
-		const struct sniff_udp *udp;
-		const char *payload;
-		
-		int size_ip;
-		int size_tcp;
-		int size_udp = 8; // fixed udp header length
-		int size_icmp = 8; // fixed udp header length
-		int size_payload;
-		
-		// extract ethernet header
-		ethernet = (struct sniff_ethernet*)(packet);
-		
-		if (ntohs(ethernet->ether_type) == ETHERTYPE_IPV4) {
-			// extract ip header
-			ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
-			size_ip = IP_HL(ip)*4;
-			if (size_ip < 20) {
-				return;
-			}
-			
-			// for tcp packet
-			if (ip->ip_p == IPPROTO_TCP) {
-				tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
-				size_tcp = TH_OFF(tcp)*4;
-				if (size_tcp < 20) {
-					return;
-				}
-				
-				// extract payload
-				payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
-				size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
-				
-				// print payload
-				if (size_payload > 0) {
-					char str_payload[size_payload];
-					strncpy(str_payload, payload, size_payload);
-					
-					if (strstr(str_payload, string) == NULL)
+					else {
+						// inject packet here
+						printf("Found matching, inject packet!\n");
+						int i = send_spoof_packet();
+						if (!i) 
+							printf("Send spoof packet error!\n");
 						return;
-					else
-						handle_packet(http, string, header, packet);
+					}
 				} else {
-					return;
-				}
-			} else if (ip->ip_p == IPPROTO_UDP) {
-				udp = (struct sniff_udp*)(packet + SIZE_ETHERNET + size_ip);
-				
-				// extract payload
-				payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_udp);
-				size_payload = ntohs(ip->ip_len) - (size_ip + size_udp);
-				
-				// print payload
-				if (size_payload > 0) {
-					char str_payload[size_payload];
-					strncpy(str_payload, payload, size_payload);
-					
-					if (strstr(str_payload, string) == NULL)
-						return;
-					else
-						handle_packet(http, string, header, packet);
-				} else {
-					return;
-				}
-			} else if (ip->ip_p == IPPROTO_ICMP) {
-				// extract payload
-				payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_icmp);
-				size_payload = ntohs(ip->ip_len) - (size_ip + size_icmp);
-				
-				// print payload
-				if (size_payload > 0) {
-					char str_payload[size_payload];
-					strncpy(str_payload, payload, size_payload);
-					
-					if (strstr(str_payload, string) == NULL)
-						return;
-					else
-						handle_packet(http, string, header, packet);
-				} else {
+					// inject packet here if no pattern specified
+					printf("No pattern, inject packet!\n");
+					int i = send_spoof_packet();
+					if (!i) 
+						printf("Send spoof packet error!\n");
 					return;
 				}
 			} else {
-				// extract payload
-				payload = (u_char *)(packet + SIZE_ETHERNET + size_ip);
-				size_payload = ntohs(ip->ip_len) - (size_ip);
-				
-				// print payload
-				if (size_payload > 0)
-				{
-					char str_payload[size_payload];
-					strncpy(str_payload, payload, size_payload);
-					
-					if (strstr(str_payload, string) == NULL)
-						return;
-					else
-						handle_packet(http, string, header, packet);
-				} else {
-					return;
-				}
+				//printf("No payload\n");
+				return;
 			}
+		} else {
+			//printf("NON-TCP packet\n");
+			return;
 		}
+		
+	} else {
+		//printf("NON-IPv4 packet\n");
+		return;
 	}
 }
 
@@ -438,7 +252,6 @@ int main(int argc, char *argv[]) {
 	char *interface = NULL;
 	char *regexp = NULL;
 	char *datafile = NULL;
-	boolean http = false;
 	char *expression = NULL;
 	// this string is used for communication between this
 	// program and pcap
@@ -447,9 +260,6 @@ int main(int argc, char *argv[]) {
 	pcap_t *handle;
 	// compiled filter
 	struct bpf_program filter;
-	// http get and post filter before compiling
-	char filter_string[] = "(tcp port http) && ((tcp[32:4] = 0x47455420) || \
-		(tcp[((tcp[12:1] & 0xf0) >> 2):4] = 0x504f5354))";
 	// The netmask of interface
 	bpf_u_int32 mask;
 	// The ip address of interface
@@ -469,7 +279,7 @@ int main(int argc, char *argv[]) {
 				break;
 			case 'r':
 				regexp = optarg;
-				//printf("file: %s\n", file);
+				//printf("file: %s\n", file);quantumdetect
 				break;
 			case 'd':
 				datafile = optarg;
@@ -505,13 +315,8 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 	
-	if (regexp == NULL) {
-		printf("Please specify the match pattern of the packets which you want to spoof response!\n");
-		return 0;
-	}
-	
 	if (datafile == NULL) {
-		printf("Please specify the data file you want to use as the spoofed payload!\n");
+		printf("Please use -d to specify the data file you want to use as the spoofed payload!\n");
 		return 0;
 	}
 	
@@ -519,23 +324,23 @@ int main(int argc, char *argv[]) {
 		interface = pcap_lookupdev(errbuf);
 		//interface = NULL;
 		if (interface == NULL) {
-			printf("Error finding default device! Error message: %s\n\
+			printf("Error getting default device! Error message: %s\n\
 			Exiting...\n", errbuf);
 			return 0;
 		}
 	}
 	
-	printf("\nInitializing quantum injector using following parameters:\n\
+	printf("\n\tInitializing quantum injector using following parameters:\n\
 		interface: %s\n\
 		regular expression: %s\n\
 		data file: %s\n\
 		expression: %s\n\n\n", interface, regexp, datafile, expression);
 	
-	// open interface or file here
-	/*if (interface != NULL && file == NULL) {
+	// open interface here
+	if (interface != NULL) {
 		// Get ip and netmask of sniffing interface or file
 		if (pcap_lookupnet(interface, &net, &mask, errbuf) == -1) {
-			printf("Error getting ip and mask! Error message: %s\n", errbuf);
+			printf("Error getting ip and mask of the interface! Error message: %s\n", errbuf);
 			net = 0;
 			mask = 0;
 		}
@@ -546,15 +351,15 @@ int main(int argc, char *argv[]) {
 			Existing...\n", errbuf);
 			return 0;
 		}
-	} else if (interface == NULL && file != NULL) {
-		handle = pcap_open_offline(file, errbuf);
-		if (handle == NULL) {
-			printf("Error opening file! Error message: %s\n\
-			Existing...\n", errbuf);
-			return 0;
-		}
 	} else {
-		printf("This shouldn't be printed out! Existing...\n");
+		printf("Interface not specified when initializing! Existing...\n");
+		return 0;
+	}
+	
+	// read payload file
+	spoof_payload = read_file(datafile);
+	if (!spoof_payload) {
+		printf("read data file failed!\n");
 		return 0;
 	}
 	
@@ -562,22 +367,6 @@ int main(int argc, char *argv[]) {
 	if (pcap_datalink(handle) != DLT_EN10MB) {
 		printf("Interface %s doesn't using ethernet header! Existing\n", interface);
 		return 0;
-	}
-	
-	// set http sniffer mode
-	if (http) {
-		// compile filter string
-		if (pcap_compile(handle, &filter, filter_string, 0, net) == -1) {
-			printf("Error compiling http filter! Error message: %s\n\
-			Existing...\n", pcap_geterr(handle));
-			return 0;
-		}
-		// apply compiled filter to session
-		if (pcap_setfilter(handle, &filter) == -1) {
-			printf("Error applying http filer! Error message: %s\n\
-			Existing...\n", pcap_geterr(handle));
-			return 0;
-		}
 	}
 	
 	// compile and apply expression
@@ -597,23 +386,10 @@ int main(int argc, char *argv[]) {
 	}
 	
 	// now we start sniffing!
-	if (http) {
-		int len = 0;
-		if (string != NULL)
-			len = strlen(string);
-		char *tmp = (char *)malloc(len+2);
-		strcpy(tmp, "g");
-		if (string != NULL)
-			strcat(tmp, string);
-		
-		//printf("Old string: %s\n", tmp);
-		pcap_loop(handle, cnt, got_packet, tmp);
-	} else {
-		pcap_loop(handle, cnt, got_packet, string);
-	}
+	pcap_loop(handle, cnt, handle_packet, regexp);
 	
 	//pcap_freecode(&filter);
 	pcap_close(handle);
 	printf("\n\t... ... the end ... ...\n\n");
-	return 0;*/
+	return 0;
 }
